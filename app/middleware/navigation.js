e6839513bc4738128419038fc30850ac51ea6a90
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-02-08 02:25:18 +0000

    Users and account

---
+++ app/middleware/navigation.js
@@ -0,0 +1,37 @@
+export const navigation = (request, response, next) => {
+  const { data } = request.session
+  const { __ } = response.locals
+
+  const accountLabel = data?.token?.email || __('account.show.title')
+
+  response.locals.navigation = {
+    // `header.primaryLinks` renders empty items, so need different arrays
+    primaryLinks: data.token?.admin
+      ? [
+          {
+            url: '/users',
+            label: __('user.list.title')
+          },
+          {
+            url: '/account',
+            label: accountLabel
+          },
+          {
+            url: '/account/sign-out',
+            label: __('account.sign-out.title')
+          }
+        ]
+      : [
+          {
+            url: '/account',
+            label: accountLabel
+          },
+          {
+            url: '/account/sign-out',
+            label: __('account.sign-out.title')
+          }
+        ]
+  }
+
+  next()
+}
---
+++ app/routes/account.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import { accountController } from '../controllers/account.js'
+
+const router = express.Router({ strict: true })
+
+router.get('/sign-in', accountController.signIn)
+router.post('/sign-in', accountController.login)
+
+router.get('/sign-out', accountController.logout)
+
+router.all('/*', accountController.read)
+
+router.get('/', accountController.edit)
+router.post('/', accountController.update)
+
+export const accountRoutes = router
