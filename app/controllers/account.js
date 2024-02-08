1abe5cbcdb5e1f8855b81363c19d0fdd6b8858b4
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-02-08 02:25:18 +0000

    Users and account

---
+++ app/controllers/account.js
@@ -0,0 +1,53 @@
+import { User } from '../models/user.js'
+
+export const accountController = {
+  signIn(request, response) {
+    response.render('account/sign-in')
+  },
+
+  login(request, response) {
+    const { email } = request.body
+    const { data } = request.session
+
+    const user = Object.values(data.users).find((user) => user.email === email)
+
+    request.session.data.token = user || Object.values(data.users).at(-1)
+    response.redirect('/dashboard')
+  },
+
+  logout(request, response) {
+    delete request.session.data.token
+
+    response.redirect('/')
+  },
+
+  edit(request, response) {
+    const { __ } = response.locals
+
+    response.render('users/edit', {
+      headingSize: 'xl',
+      paths: false,
+      title: __('account.show.title')
+    })
+  },
+
+  read(request, response, next) {
+    const { data } = request.session
+
+    response.locals.user = data.token && new User(data.token)
+
+    next()
+  },
+
+  update(request, response) {
+    const { __, user } = response.locals
+
+    request.session.data.users[user.uuid] = new User({
+      ...user,
+      ...request.body.user
+    })
+
+    request.flash('success', __(`account.success.update`))
+    response.redirect(`/dashboard`)
+  }
+}
