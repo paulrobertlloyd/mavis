cff81a23523ad314352f5ae42663ec2d81f4ba86
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-02-13 22:47:22 +0000

    Reports

--- app/routes.js
+++ app/routes.js
@@ -8,6 +8,7 @@ import { accountRoutes } from './routes/account.js'
 import { batchRoutes } from './routes/batch.js'
 import { campaignRoutes } from './routes/campaign.js'
 import { recordRoutes } from './routes/record.js'
+import { reportRoutes } from './routes/report.js'
 import { sessionRoutes } from './routes/session.js'
 import { userRoutes } from './routes/user.js'
 import { vaccineRoutes } from './routes/vaccine.js'
@@ -21,6 +22,7 @@ router.use(flash(), navigation, notification)
 router.use('/account', accountRoutes)
 router.use('/campaigns', campaignRoutes)
 router.use('/records', recordRoutes)
+router.use('/reports', reportRoutes)
 router.use('/sessions', sessionRoutes)
 router.use('/users', userRoutes)
 router.use('/vaccines', vaccineRoutes)
