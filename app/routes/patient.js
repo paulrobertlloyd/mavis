43b8e24d5f5f2b8a65cab6a1f8e1e73dc3384db8
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-03-14 14:29:05 +0000

    Gillick assessment

--- app/routes/patient.js
+++ app/routes/patient.js
@@ -3,7 +3,10 @@ import { patientController } from '../controllers/patient.js'
 
 const router = express.Router({ strict: true, mergeParams: true })
 
+router.all('/*', patientController.read)
+
 router.get('/', patientController.show)
+
 router.get('/log', patientController.log)
 
 export const patientRoutes = router
