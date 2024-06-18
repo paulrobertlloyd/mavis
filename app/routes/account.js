273c85b91e14e40a8bbec182fa7b5e9686176cb5
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-06-18 20:11:12 +0100

    Login with CIS2

--- app/routes/account.js
+++ app/routes/account.js
@@ -6,7 +6,7 @@ const router = express.Router({ strict: true })
 router.get('/reset-password', accountController.resetPassword)
 
 router.get('/sign-in', accountController.signIn)
-router.post('/sign-in', accountController.login)
+router.post(['/sign-in', '/cis2'], accountController.login)
 
 router.get('/sign-out', accountController.logout)
 
